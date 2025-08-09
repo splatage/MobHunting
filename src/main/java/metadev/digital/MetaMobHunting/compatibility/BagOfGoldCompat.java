9b07b700a766a92bd496dea9e3897851990777b5
kevin-sherman <kevin@metadevdigital.com>
2025-08-09 14:06:32 -0400

    BagOfGold: Implement ICompat and IFeatureHolder

--- src/main/java/metadev/digital/MetaMobHunting/compatibility/BagOfGoldCompat.java
+++ src/main/java/metadev/digital/MetaMobHunting/compatibility/BagOfGoldCompat.java
@@ -1,6 +1,15 @@
 package metadev.digital.MetaMobHunting.compatibility;
 
 import metadev.digital.MetaMobHunting.Messages.MessageHelper;
+import metadev.digital.MetaMobHunting.MobHunting;
+import metadev.digital.metacustomitemslib.compatibility.Feature;
+import metadev.digital.metacustomitemslib.compatibility.FeatureList;
+import metadev.digital.metacustomitemslib.compatibility.ICompat;
+import metadev.digital.metacustomitemslib.compatibility.IFeatureHolder;
+import metadev.digital.metacustomitemslib.compatibility.enums.BoundIdentifierEnum;
+import metadev.digital.metacustomitemslib.compatibility.enums.VersionSetIdentifierEnum;
+import metadev.digital.metacustomitemslib.compatibility.exceptions.FeatureNotFoundException;
+import metadev.digital.metacustomitemslib.compatibility.exceptions.SpinupShutdownException;
 import org.bukkit.Bukkit;
 
 import net.citizensnpcs.api.CitizensAPI;
@@ -9,62 +18,174 @@ import metadev.digital.metabagofgold.bank.BankManager;
 import metadev.digital.metabagofgold.storage.DataStoreManager;
 import metadev.digital.metacustomitemslib.Core;
 import metadev.digital.metacustomitemslib.compatibility.enums.SupportedPluginEntities;
-import metadev.digital.MetaMobHunting.MobHunting;
+import org.bukkit.plugin.Plugin;
 
-public class BagOfGoldCompat {
+public class BagOfGoldCompat implements ICompat, IFeatureHolder {
 
-	private BagOfGold mPlugin;
-	private static boolean supported = false;
-	private final String latestSupported = "4.5.7";
+    // ****** Standard ******
+    private Plugin compatPlugin;
+    private static boolean enabled = false, supported = false, loaded = false;
+    private static String sMin, sMax, pMin = "4.5.7", pMax;
+    private static FeatureList features;
 
-	public BagOfGoldCompat() {
-		mPlugin = (BagOfGold) Bukkit.getPluginManager().getPlugin(SupportedPluginEntities.BagOfGold.getName());
+    // ****** Plugin Specific ******
 
-		if (mPlugin.getDescription().getVersion().compareTo(latestSupported) >= 0) {
-			MessageHelper.notice("Enabling compatibility with BagOfGold ("
-							+ getBagOfGoldAPI().getDescription().getVersion() + ")");
-			supported = true;
-		} else {
-			MessageHelper.warning("Your current version of BagOfGold ("
-							+ mPlugin.getDescription().getVersion()
-							+ ") is not supported by MobHunting. Please upgrade to " + latestSupported + " or newer.");
-			Bukkit.getPluginManager().disablePlugin(mPlugin);
-		}
+    public BagOfGoldCompat() {
+        compatPlugin = Bukkit.getPluginManager().getPlugin(SupportedPluginEntities.BagOfGold.getName());
 
-	}
+        if(compatPlugin != null) {
+            try {
+                start();
+            } catch (SpinupShutdownException e) {
+                Bukkit.getPluginManager().disablePlugin(compatPlugin);
+            }
+        }
+    }
 
-	// **************************************************************************
-	// OTHER
-	// **************************************************************************
+    // ****** ICompat ******
 
-	public BagOfGold getBagOfGoldAPI() {
-		return mPlugin;
-	}
+    @Override
+    public void start() throws SpinupShutdownException {
+        detectedMessage();
+        registerFeatures();
 
-	public static boolean isSupported() {
-		return supported;
-	}
+        if (isActive()) {
+            successfullyLoadedMessage();
+            loaded = true;
+        } else if (enabled && !supported) {
+            Feature base = getFeature("base");
+            if(base != null) unsupportedMessage(base);
+            else pluginError("Plugin is enabled but not supported, and failed to understand the reasoning out of the base " +
+                    "feature. Likely caused by a corrupt / incorrect construction of the base feature.");
+            throw new SpinupShutdownException();
+        }
+    }
 
-	public static boolean useAsEconomyAnEconomyPlugin() {
-		return supported && BagOfGold.getInstance().getConfigManager().useBagOfGoldAsAnEconomyPlugin;
-	}
+    @Override
+    public void shutdown() throws SpinupShutdownException {
+        if (isActive() && loaded) {
+            successfullyShutdownMessage();
+            loaded = false;
+        }
+    }
 
-	public String getBagOfGoldFormat() {
-		return Core.getConfigManager().numberFormat;
-	}
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 
-	public DataStoreManager getDataStoreManager() {
-		return BagOfGold.getInstance().getDataStoreManager();
-	}
+    @Override
+    public boolean isSupported() {
+        return supported;
+    }
 
-	public BankManager getBankManager() {
-		return BagOfGold.getInstance().getBankManager();
-	}
+    @Override
+    public boolean isActive() {
+        return enabled && supported;
+    }
 
-	public static boolean isNPC(Integer id) {
-		if (isSupported())
-			return CitizensAPI.getNPCRegistry().getById(id) != null;
-		return false;
-	}
+    @Override
+    public boolean isLoaded() {
+        return loaded;
+    }
 
+    @Override
+    public Plugin getPluginInstance() {
+        return compatPlugin;
+    }
+
+    @Override
+    public String getPluginName() {
+        return compatPlugin.getName();
+    }
+
+    @Override
+    public String getPluginVersion() {
+        return compatPlugin.getDescription().getVersion();
+    }
+
+    // ****** IFeatureHolder ******
+
+    @Override
+    public void registerFeatures() {
+        features = new FeatureList(getPluginVersion());
+
+        // Base plugin
+        enabled = MobHunting.getInstance().getConfigManager().enableIntegrationBagOfGold;
+        features.addFeature("base", pMin, BoundIdentifierEnum.FLOOR, VersionSetIdentifierEnum.PLUGIN, enabled);
+        supported = isFeatureSupported("base");
+
+        // Other features
+    }
+
+    @Override
+    public boolean isFeatureEnabled(String name) {
+        boolean featureEnabled = false;
+        try {
+            featureEnabled = features.isFeatureEnabled(name);
+        } catch (FeatureNotFoundException e) {
+            MessageHelper.debug("Triggered a FeatureNotFoundException when trying to return enable flag of the feature " + name + " in the " + compatPlugin.getName() +" compat class." );
+        }
+
+        return featureEnabled;
+    }
+
+    @Override
+    public boolean isFeatureSupported(String name) {
+        boolean featureSupported = false;
+        try {
+            featureSupported = features.isFeatureSupported(name);
+        } catch (FeatureNotFoundException e) {
+            MessageHelper.debug("Triggered a FeatureNotFoundException when trying to return supported flag of the feature " + name + " in the " + compatPlugin.getName() +" compat class." );
+        }
+
+        return featureSupported;
+    }
+
+    @Override
+    public boolean isFeatureActive(String name) {
+        boolean featureActive = false;
+        try {
+            featureActive = features.isFeatureActive(name);
+        } catch (FeatureNotFoundException e) {
+            MessageHelper.debug("Triggered a FeatureNotFoundException when trying to return active flag of the feature " + name + " in the " + compatPlugin.getName() +" compat class." );
+        }
+
+        return featureActive;
+    }
+
+    @Override
+    public Feature getFeature(String name) {
+        Feature feature;
+        try {
+            feature = features.getFeature(name);
+            return feature;
+        } catch (FeatureNotFoundException e) {
+            MessageHelper.debug("Triggered a FeatureNotFoundException when trying to return the feature " + name + " in the " + compatPlugin.getName() +" compat class." );
+        }
+        return null;
+    }
+
+    // ****** Plugin Specific ******
+    public static boolean useBagOfGoldAsAnEconomyPlugin() {
+        return MobHunting.getInstance().getCompatibilityManager().isCompatibilityLoaded(Bukkit.getPluginManager().getPlugin(SupportedPluginEntities.BagOfGold.getName())) && BagOfGold.getInstance().getConfigManager().useBagOfGoldAsAnEconomyPlugin;
+    }
+
+    public String getBagOfGoldFormat() {
+        return Core.getConfigManager().numberFormat;
+    }
+
+    public DataStoreManager getDataStoreManager() {
+        return BagOfGold.getInstance().getDataStoreManager();
+    }
+
+    public BankManager getBankManager() {
+        return BagOfGold.getInstance().getBankManager();
+    }
+
+    public static boolean isNPC(Integer id) {
+        if (MobHunting.getInstance().getCompatibilityManager().isCompatibilityLoaded(Bukkit.getPluginManager().getPlugin(SupportedPluginEntities.BagOfGold.getName())))
+            return CitizensAPI.getNPCRegistry().getById(id) != null;
+        return false;
+    }
 }
